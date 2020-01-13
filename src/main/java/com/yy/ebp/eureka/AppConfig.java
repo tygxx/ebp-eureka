39f0b1b3b75bfa84823841b60a4f3c4e808a0e6b
tygxx <251380405@qq.com>
2020-01-13 17:58:09 +0800

    Initial commit

---
+++ src/main/java/com/yy/ebp/eureka/AppConfig.java
@@ -0,0 +1,22 @@
+package com.yy.ebp.eureka;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	
+//	@Value("${dashboard.enable}")
+//	private Boolean dashboardEnable;
+//
+//	@Bean
+//	public FilterRegistrationBean userSessionFilter() {
+//		FilterRegistrationBean registration = new FilterRegistrationBean();
+//		registration.setName("dashboardFilter");
+//		DashboardFilter dashboardFilter = new DashboardFilter(dashboardEnable);
+//		registration.setFilter(dashboardFilter);
+//		registration.addUrlPatterns("/*");
+//		registration.setOrder(FilterRegistrationBean.REQUEST_WRAPPER_FILTER_MAX_ORDER);
+//
+//		return registration;
+//	}
+}
